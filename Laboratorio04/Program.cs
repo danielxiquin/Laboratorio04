d86fcc4b4b844488ceb7882b83d0abc0e5172c23
Daniel Xiquin <danielxiquin802@gmail.com>
2023-03-18 11:27:18 -0600

    Resolver ejercicio 1

--- Laboratorio04/Program.cs
+++ Laboratorio04/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Nombre: ");
-            Console.WriteLine("Carnet: ");
-            Console.WriteLine("Seccion: ");
+            Console.WriteLine("Nombre: Oscar Daniel Xiquin Cumes ");
+            Console.WriteLine("Carnet: 1118423 ");
+            Console.WriteLine("Seccion: 04 ");
         }
     }
 }
